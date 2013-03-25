e15efe684f914dcf1ec65558ef65adcffce26a4b
jpedro <jpsfmmb@gmail.com>
2013-03-25 14:50:50 -0300

    script de criacao da Base de Dados

--- doc/script_escola.sql
+++ doc/script_escola.sql
@@ -1,11 +1,3 @@
-CREATE DATABASE escola_development
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'pt_BR.UTF-8'
-       LC_CTYPE = 'pt_BR.UTF-8'
-       CONNECTION LIMIT = -1;
-
 CREATE TABLE alunos
 (
   id serial NOT NULL,
